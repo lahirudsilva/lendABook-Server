0986bf8d6cd2cd201639922cc84758ac6b2a1ca9
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-09 14:56:51 +0530

    [+] user route added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 // allows api for parsing json
-app.use(express.json()); 
+app.use(express.json());
 
-const booksRoute = require('./routes/books');
+/*User Routes*/
+const userRoute = require("./routes/users");
+
+/*Book Routes*/
+const booksRoute = require("./routes/books");
 app.use("/books", booksRoute);
+app.use("/user", userRoute);
 
 module.exports = app;
