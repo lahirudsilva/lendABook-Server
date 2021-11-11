2d0a9bc48926f60df70dd81e5c4d5e9a1ca437a9
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-12 02:09:35 +0530

    [+] minor changes

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const http = require('http');
-const app =require('./app');
-const cors = require("cors");
+const http = require("http");
+const app = require("./app");
+
 require("dotenv").config();
 
 //api for cross-origin resource sharing
-app.use(cors()); 
 
 const db = require("./models");
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 const port = process.env.PORT || 3001;
 
-
-db.sequelize.sync().then(() => {
+db.sequelize
+  .sync()
+  .then(() => {
     server.listen(port, () => {
-        console.log(`server is running on PORT:${port}`);
+      console.log(`server is running on PORT:${port}`);
     });
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(`Error connecting : ${err.message}`);
-})
-
+  });
