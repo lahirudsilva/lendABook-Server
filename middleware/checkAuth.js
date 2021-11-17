339dfbd8090fddeef7117a56b5853036f1a29ee7
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-18 00:16:42 +0530

    [+] auth middleware

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,50 +1,48 @@
 const jwt = require("jsonwebtoken");
-const models = require("../models/user");
+const models = require("../models");
 
-function checkAuth(req, res, next) {
-  // try {
-  //   const token = req.headers.authorization.split("Bearer ")[1]; // bearer
-  //   const decodedToken = jwt.verify(token, process.env.JWT_KEY);
-  //   req.userDate = decodedToken;
-  //   next();
-  // } catch (error) {
-  //   return res.status(401).json({
-  //     message: "Invalid or expired token provided!",
-  //     error: error,
-  //   });
-  // }
+module.exports = (admin) => async (req, res, next) => {
+  let token;
 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer ")
   ) {
-    const token = req.headers.authorization.split("Bearer ")[1]; // bearer
-    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
-
-    if (decodedToken === null) {
-      return res.status(403).json({
-        message: "No token found!",
-        error: "Unauthoraized",
-      });
-    } else {
-      req.userData = decodedToken;
-      return next();
-    }
+    token = req.headers.authorization.split("Bearer ")[1]; // bearer
   } else {
-    return res.status(403).json({
-      message: "Invalid or expired token provided!",
-      error: "Unauthoraized",
-    });
+    return res.status(403).json({ error: "Unauthoraized" });
   }
 
-  // } catch (error) {
-  //   return res.status(401).json({
-  //     message: "Invalid or expired token provided!",
-  //     error: "Unauthoraized",
-  //   });
-  // }
-}
+  let auth_token;
 
-module.exports = {
-  checkAuth: checkAuth,
+  jwt.verify(token, process.env.JWT_KEY, (error, decodedToken) => {
+    auth_token = decodedToken;
+  });
+
+  //If token is not succesfully decoded
+  if (!auth_token) return res.status(403).json({ error: "Unauthoraized" });
+
+  // console.log(auth_token.email);
+  let user;
+
+  try {
+    //Check if user exists
+    user = await models.User.findOne({ where: { email: auth_token.email } });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  //If user doesnt exist send error response
+  if (!user) return res.status(403).json({ error: "Unauthoraized" });
+
+  //If using as admin authorization middleware. check if user is admin
+  if (admin === "admin") {
+    if (user.role !== "admin") {
+      return res.status(403).json({ error: "Unauthoraized" });
+    }
+  }
+
+  req.userData = user;
+
+  return next();
 };
