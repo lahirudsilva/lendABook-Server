65f100e2beb881d496b62eb1f3c22ef7c2bd4d17
lahiru de silva <desilvalahiru4@gmail.com>
2021-12-12 22:09:29 +0530

    [+] routes - register, login, currentuser...

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,28 @@
 const express = require("express");
-const userController = require("../controllers/user.controller");
+const userController = require("../controllers/controller");
 const checkAuthMiddleware = require("../middleware/checkAuth");
+const {
+  signUp,
+  login,
+  getUsers,
+  getUser,
+  changeIsBlacklisted,
+  changeIsVerified,
+  getLoggedInUser,
+} = require("../controllers/user.controller");
 
 const router = express.Router();
 
-router.post("/register", userController.signUp);
-router.post("/login", userController.login);
+router.post("/register", signUp);
+router.post("/login", login);
+router.get("/LoggedUser", checkAuthMiddleware(), getLoggedInUser);
+router.get("/", checkAuthMiddleware("admin"), getUsers); //Get all users in the system
+router.get("/:id", checkAuthMiddleware(), getUser); //Get information about a specific user
 router.get(
-  "/LoggedUser",
-  checkAuthMiddleware.checkAuth,
-  userController.getLoggedUser
-);
+  "/set-blacklisted/:id",
+  checkAuthMiddleware("admin"),
+  changeIsBlacklisted
+); //Change isBlacklisted property of selected user
+router.get("/set-verified/:id", checkAuthMiddleware("admin"), changeIsVerified); //Change isVerified property of selected user
 
 module.exports = router;
