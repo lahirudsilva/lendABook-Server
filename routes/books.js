0392c766f6dc5219290ff924856a4b8c589322a6
lahiru de silva <desilvalahiru4@gmail.com>
2021-12-12 22:10:34 +0530

    [+] routes - add, getbook, add bookimage...

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const booksController = require('../controllers/book.controller');
+const express = require("express");
+const {
+  addBooks,
+  uploadBookImage,
+  getAllBooks,
+  getBook,
+  toggleAvailability,
+  getAvailableBooks,
+  
+} = require("../controllers/book.controller");
+const checkAuthMiddleware = require("../middleware/checkAuth");
 
 const router = express.Router();
 
-router.post("/", booksController.addBooks);
-router.get("/:ISBN", booksController.getBook);
-router.get("/", booksController.getAllBooks);
-router.patch("/:ISBN", booksController.updateBookDetails);
-router.delete("/:ISBN", booksController.deleteBook);
+router.post("/add-book", checkAuthMiddleware("admin"), addBooks);
+router.get("/:ISBN", checkAuthMiddleware(), getBook);
+router.get("/", checkAuthMiddleware(), getAllBooks);
+// router.patch("/:ISBN", booksController.updateBookDetails);
+// router.delete("/:ISBN", booksController.deleteBook);
+router.post("/book-image/:ISBN", checkAuthMiddleware("admin"), uploadBookImage);
+router.get("/book-availability/:ISBN",checkAuthMiddleware("admin"), toggleAvailability);
+router.get("/available-books/:reserveDate/:returnDate", getAvailableBooks);
 
 module.exports = router;
