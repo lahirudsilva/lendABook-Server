e8b90a951435b57e89be5909cbf1b8b2abe78ffb
lahiru de silva <desilvalahiru4@gmail.com>
2021-12-30 21:41:24 +0530

    [+]manage  reservation

--- controllers/reservation.controller.js
+++ controllers/reservation.controller.js
@@ -1,3 +1,5 @@
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const models = require("../models");
 
 exports.reserveBook = async (req, res) => {
@@ -38,4 +40,228 @@ exports.reserveBook = async (req, res) => {
     // If everything goes well, respond with the order
     return res.status(200).json({ message: "Reservation succeeded!" });
   });
+};
+
+exports.reserveMovie = async (req, res) => {
+  // Create and save the order
+
+  const newReservation = {
+    reserveDate: req.body.reserve,
+    returnDate: req.body.returnDate,
+    status: "pending",
+    charge: req.body.charge,
+    UserId: req.body.userId,
+  };
+  console.log(newReservation);
+  console.log(req.body.movies);
+  const savedReservation = await models.Reservation.create(newReservation);
+
+  req.body.movies.cartItems.forEach(async (item) => {
+    // Search for the book with the givenId and make sure it exists. If it doesn't, respond with status 400.
+
+    const _movie = await models.Movie.findByPk(item.movieId);
+    if (!_movie) {
+      res.status(400).json({ error: error.message });
+      return;
+    }
+
+    // Create a dictionary with which to create the bookreservation
+    const po = {
+      reservatonId: savedReservation.id,
+      movieId: item.movieId,
+    };
+
+    // Create and save a BookReservation
+    const savedBookReservation = await models.MovieReservation.create(
+      po
+      // { w: 1 },
+      // { returning: true }
+    );
+    // If everything goes well, respond with the order
+    return res.status(200).json({ message: "Reservation succeeded!" });
+  });
+};
+
+exports.getBookReservations = async (req, res) => {
+  try {
+    const reservations = await models.Reservation.findAll({
+      // Make sure to include the books
+      include: [
+        // {
+        //   model: models.User,
+        //   as: "user",
+        // },
+        {
+          model: models.Book,
+          as: "books",
+          required: true,
+
+          // Pass in the book attributes that you want to retrieve
+          attributes: [
+            "ISBN",
+            "title",
+            "bookCover",
+            "summary",
+            "category",
+            "author",
+            "publisher",
+            "isAvailable",
+            "rating",
+          ],
+
+          through: {
+            model: models.BookReservation,
+            as: "booksReservations",
+            attributes: ["reservatonId", "bookId"],
+          },
+        },
+      ],
+    });
+
+    return res.status(200).json({ reservations });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getMovieReservations = async (req, res) => {
+  try {
+    const reservations = await models.Reservation.findAll({
+      include: [
+        {
+          model: models.Movie,
+          as: "movies",
+          required: true,
+
+          // Pass in the movie attributes that you want to retrieve
+          attributes: [
+            "movieId",
+            "title",
+            "movieCover",
+            "summary",
+            "category",
+            "director",
+            "production",
+            "isAvailable",
+            "isAdultOnly",
+            "rating",
+          ],
+          through: {
+            model: models.MovieReservation,
+            as: "moviesReservations",
+            attributes: ["reservatonId", "movieId"],
+          },
+        },
+      ],
+    });
+    return res.status(200).json({ reservations });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.setReservationsStatus = async (req, res) => {
+  const id = req.params.id;
+  const status = req.body.status;
+
+  try {
+    const reservation = await models.Reservation.findByPk(id);
+
+    //If rent is not found
+    if (!reservation)
+      return res.status(404).json({ error: "Reservation not found." });
+
+    //If invalid status
+    if (status !== "pending" && status !== "collected" && status !== "returned")
+      res.status(400).json({ error: "Invalid status." });
+
+    //Set new status and save
+    reservation.status = status;
+    reservation.save();
+
+    return res.status(200).json({ message: "Successfully changed status" });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+exports.getMyBookReservations = async (req, res) => {
+  const id = req.userData.dataValues.id;
+  try {
+    const reservations = await models.Reservation.findAll({
+      where: { UserId: id },
+      include: [
+        {
+          model: models.Book,
+          as: "books",
+          required: true,
+
+          // Pass in the book attributes that you want to retrieve
+          attributes: [
+            "ISBN",
+            "title",
+            "bookCover",
+            "summary",
+            "category",
+            "author",
+            "publisher",
+            "isAvailable",
+            "rating",
+          ],
+
+          through: {
+            model: models.BookReservation,
+            as: "bookReservations",
+            attributes: ["reservatonId", "bookId"],
+          },
+        },
+      ],
+    });
+
+    const _reservations = [...reservations];
+    return res.status(200).json({ reservations: _reservations });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+exports.getMyMovieReservations = async (req, res) => {
+  const id = req.userData.dataValues.id;
+  try {
+    const reservations = await models.Reservation.findAll({
+      where: { UserId: id },
+      include: [
+        {
+          model: models.Movie,
+          as: "movies",
+          required: true,
+
+          // Pass in the book attributes that you want to retrieve
+          attributes: [
+            "movieId",
+            "title",
+            "movieCover",
+            "summary",
+            "category",
+            "director",
+            "production",
+            "isAvailable",
+            "isAdultOnly",
+            "rating",
+          ],
+
+          through: {
+            model: models.MovieReservation,
+            as: "movieReservations",
+            attributes: ["reservatonId", "movieId"],
+          },
+        },
+      ],
+    });
+
+    const _reservations = [...reservations];
+    return res.status(200).json({ reservations: _reservations });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
